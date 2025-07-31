1759ab1b415fb9ce41379f395656d2d0e630c09b
majharul-web <majharuljony163@gmail.com>
2025-07-31 21:15:52 +0600

    cart item update delete

--- order/serializers.py
+++ order/serializers.py
@@ -6,7 +6,43 @@ from product.models import Product
 class SimpleProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price']  # Adjust fields as necessary
+        fields = ['id', 'name', 'price']  
+
+class AddCartItemSerializer(serializers.ModelSerializer):
+    product_id = serializers.IntegerField(write_only=True)
+    class Meta:
+        model = CartItem
+        fields = ['product_id', 'quantity']
+    
+    def save(self, **kwargs):
+        cart_id = self.context['cart_id']
+        product_id = self.validated_data['product_id']
+        quantity = self.validated_data['quantity']
+
+        try:
+            cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
+            cart_item.quantity += quantity
+            cart_item.save()
+            self.instance = cart_item
+        except CartItem.DoesNotExist:
+            cart_item = CartItem(cart_id=cart_id, **self.validated_data)
+            cart_item.save()  
+            self.instance = cart_item
+
+        return self.instance
+
+    def validate_product_id(self, attrs):
+        if not Product.objects.filter(id=attrs).exists():
+            raise serializers.ValidationError("Product does not exist.")
+        return attrs
+    
+    
+class UpdateCartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['quantity']
+
+
 class CartItemSerializer(serializers.ModelSerializer):
     product=SimpleProductSerializer(read_only=True)
     total_price = serializers.SerializerMethodField(method_name='get_total_price')
--- order/views.py
+++ order/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import GenericViewSet,ModelViewSet
 from order.models import Cart, CartItem
-from order.serializers import CartSerializer
-from order.serializers import CartSerializer, CartItemSerializer
+from order.serializers import CartSerializer, CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer
 from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, DestroyModelMixin,RetrieveModelMixin
 
 # Create your views here.
@@ -13,8 +12,17 @@ class CartViewSet(CreateModelMixin,RetrieveModelMixin,DestroyModelMixin,GenericV
 
 
 class CartItemViewSet(ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete']
     queryset = CartItem.objects.all()
-    serializer_class = CartItemSerializer
+    def get_serializer_class(self):
+        if self.request.method in ['POST']:
+            return AddCartItemSerializer
+        elif self.request.method in 'PATCH':
+            return UpdateCartItemSerializer
+        return CartItemSerializer
+
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}
     
     def get_queryset(self):
         return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
