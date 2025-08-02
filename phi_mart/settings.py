39492eb3f778e1756bb7cc828cac7c2abf575ec4
majharul-web <majharuljony163@gmail.com>
2025-08-02 16:35:08 +0600

    setup djosoer

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',   
     'rest_framework',
+    'djoser',
     'api',
     'product',
     'order',
@@ -137,7 +138,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     # 'PAGE_SIZE': 10,
     
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
 }
