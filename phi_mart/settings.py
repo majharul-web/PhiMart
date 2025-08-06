a6bde17806259130b38f02da0b4aa9f204e75da5
majharul-web <majharuljony163@gmail.com>
2025-08-06 22:32:27 +0600

    setup psql database and load data

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 from datetime import timedelta
 
@@ -89,10 +89,21 @@ WSGI_APPLICATION = 'phi_mart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('dbname', default='postgres'),
+        'USER': config('user', default='postgres'),
+        'PASSWORD': config('password', default=''),
+        'HOST': config('host', default='localhost'),
+        'PORT': config('port', default='5432'),
     }
 }
 
